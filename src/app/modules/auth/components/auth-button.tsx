7466464d6248209878d2ea813e4b8515cd06e28c
quantum_gecko <galang.izqd@gmail.com>
2025-04-17 14:03:10 +0700

    Top bar UI component

---
+++ src/app/modules/auth/components/auth-button.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button"
+import { UserCircleIcon } from "lucide-react"
+
+export const AuthButton = () => {
+    // TODO: Add different auth states
+    return (
+        <Button variant="outline"
+         className="px-4 py-2 text-sm font-medium text-blue-600 hover:text-blue-500 border-blue-500/20 rounded-full shadow-none [&_svg]:size-4">
+            <UserCircleIcon/>
+            Sign in
+        </Button>
+    )
+}
--- src/app/modules/home/ui/components/home-navbar/index.tsx
+++ src/app/modules/home/ui/components/home-navbar/index.tsx
@@ -1,6 +1,10 @@
 import { SidebarTrigger } from '@/components/ui/sidebar';
+
 import Image from 'next/image';
 import Link from 'next/link';
+
+import { SearchInput } from './search-input';
+import { AuthButton } from '@/app/modules/auth/components/auth-button';
 
 export const HomeNavbar = () => {
 	return (
@@ -10,14 +14,21 @@ export const HomeNavbar = () => {
 				<div className="flex items-center flex-shrink-0">
 					<SidebarTrigger />
 					<Link href="/">
-						<div className='p-4 flex items-center gap-1'>
+						<div className="p-4 flex items-center gap-1">
 							<Image src="/logo.svg" height={32} width={32} alt="logo" />
 							<p className="text-xl font-semibold tracking-tight">NewTube</p>
 						</div>
 					</Link>
 				</div>
 
-                {/* Search Bar */}
+				{/* Search Bar */}
+				<div className="flex flex-1 justify-center max-w-[720px] mx-auto">
+					<SearchInput />
+				</div>
+
+				<div className="flex-shrink-0 items-center flex gap-4">
+					<AuthButton />
+				</div>
 			</div>
 		</nav>
 	);
