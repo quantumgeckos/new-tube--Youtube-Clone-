eeeefe7e7ab66c3e3c45d393faa8b713218826c8
quantum_gecko <galang.izqd@gmail.com>
2025-04-17 13:34:09 +0700

    Init implement topbar & icon app

---
+++ src/app/(home)/layout.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { HomeLayout } from '../modules/home/ui/layouts/home-layout';
+
+
+interface LayoutProps {
+	children: React.ReactNode;
+}
+
+const Layout = ({ children }: LayoutProps) => {
+	return (
+        <HomeLayout>
+            {children}
+        </HomeLayout>
+    );
+};
+
+export default Layout;
